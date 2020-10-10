c1364be8a1e36bc42860450e62943de8a0efcfdd
guptarahul15 <rahulgupta@rogers.com>
2020-10-10 06:03:15 -0400

    added models and routes

---
+++ backend/models/event.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamp: true,
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
